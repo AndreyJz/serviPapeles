fbdf212dacaf53a45e08fde23433223274674da6
AndreyJz <jerez.01.18@gmail.com>
2024-07-06 21:38:08 -0500

    feat: :sparkles: login-box

--- app.js
+++ app.js
@@ -1,38 +1,101 @@
-const upButton = document.getElementById('up');
-const downButton = document.getElementById('down');
-const alturaInput = document.getElementById('altura');
-const anchoInput = document.getElementById('ancho');
-const cantidadInput = document.getElementById('cantidad');
-
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener todos los botones de clase 'up-button'
-    const upButtons = document.querySelectorAll('.up');
-  
-    // Función para manejar clic en botones de flecha hacia arriba
-    function handleClickUp(event) {
-      // Obtener el contenedor padre del botón clickeado
+
+  const articles = document.querySelectorAll('article');
+  const formContainer = document.getElementById('formContainer');
+
+  // Función para manejar clic en un article
+  function handleArticleClick(event) {
+      // Mostrar el formulario completo
+      formContainer.style.display = 'block';
+  }
+
+  // Agregar event listener a cada article
+  articles.forEach(article => {
+      article.addEventListener('click', handleArticleClick);
+  });
+
+  // Función para manejar clic en botones de flecha hacia arriba
+  function handleClickUp(event) {
+      event.preventDefault();
       const container = event.target.closest('.user-box');
-  
-      // Identificar el campo de entrada dentro del contenedor
       let input;
       if (container) {
-        input = container.querySelector('input');
+          input = container.querySelector('input');
       }
-  
+      if (input.value === "") {
+        input.value = 0.5;
+      }
       if (input) {
-        // Obtener el valor actual del input y convertirlo a número
-        let currentValue = parseFloat(input.value);
-  
-        // Sumar 0.5 al valor actual
-        currentValue += 0.5;
-  
-        // Actualizar el valor del input
-        input.value = currentValue.toFixed(1); // Para mostrar solo un decimal
+          let currentValue = parseFloat(input.value);
+
+          // currentValue += 0.5;
+
+          const decimalPart = currentValue - Math.floor(currentValue);
+          if (decimalPart !== 0 && decimalPart !== 0.5) {
+            currentValue = roundToNearest(currentValue, decimalPart);
+          } else {
+            currentValue += 0.5;
+          }
+          input.value = currentValue.toFixed(1);
+          
+
       }
-    }
-  
-    // Agregar event listener a cada botón de flecha hacia arriba
-    upButtons.forEach(function(button) {
+  }
+
+  // Función para manejar clic en botones de flecha hacia abajo
+  function handleClickDown(event) {
+      event.preventDefault();
+      const container = event.target.closest('.user-box');
+      let input;
+      if (container) {
+          input = container.querySelector('input');
+      }
+      if (input) {
+        if (input.value === "") {
+          input.value = 0.0;
+        }
+          let currentValue = parseFloat(input.value);
+          
+          const decimalPart = currentValue - Math.floor(currentValue);
+          if (decimalPart !== 0 && decimalPart !== 0.5) {
+            currentValue = roundToNearest(currentValue, decimalPart);
+          } else {
+            currentValue -= 0.5;
+          }
+
+          if (currentValue <= 0) {
+              currentValue = 0;
+          }
+          
+          input.value = currentValue.toFixed(1);
+      }
+  }
+
+  function roundToNearest(value, decimal) {
+    const rounded = Math.round(value);
+
+    if (decimal < 0.25) {
+        return Math.floor(value); // Redondea hacia abajo si la parte decimal es menor que 0.25
+    } else if (decimal > 0.75) {
+        return Math.ceil(value); // Redondea hacia arriba si la parte decimal es mayor que 0.75
+    } else if (decimal > 0.25 && decimal < 0.5) {
+        return Math.floor(value) + 0.5; // Redondea al entero más cercano si está entre 0.25 y 0.75
+    } else if (decimal < 0.75 && decimal > 0.5) {
+        return Math.ceil(value) - 0.5; // Redondea al entero más cercano si está entre 0.25 y 0.75
+  }
+}
+
+  // Obtener todos los botones de clase 'up' y 'down'
+  const upButtons = document.querySelectorAll('.up');
+  const downButtons = document.querySelectorAll('.down');
+
+  // Agregar event listener a cada botón de flecha hacia arriba
+  upButtons.forEach(function(button) {
       button.addEventListener('click', handleClickUp);
-    });
   });
+
+  // Agregar event listener a cada botón de flecha hacia abajo
+  downButtons.forEach(function(button) {
+      button.addEventListener('click', handleClickDown);
+  });
+});
