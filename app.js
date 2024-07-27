24ea106ea5c7fbbf6f528b2b26cfb8b9e824e70c
AndreyJz <jerez.01.18@gmail.com>
2024-07-27 15:09:04 -0500

    style: :lipstick: Main Page

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
   const articles = document.querySelectorAll('article');
   const formContainer = document.getElementById('formContainer');
 
-  // Función para manejar clic en un article
   function handleArticleClick(event) {
-      // Mostrar el formulario completo
-      formContainer.style.display = 'block';
+      formContainer.classList.add('active');
   }
 
-  // Agregar event listener a cada article
+  function handleClickOutside(event) {
+    if (formContainer.contains(event.target)) {
+      formContainer.classList.remove('active');
+    }
+  }
+
+  formContainer.addEventListener('click', handleClickOutside);
+
   articles.forEach(article => {
-      article.addEventListener('click', handleArticleClick);
+      article.addEventListener('click', handleArticleClick);  
   });
 
-  // Función para manejar clic en botones de flecha hacia arriba
   function handleClickUp(event) {
       event.preventDefault();
       const container = event.target.closest('.user-box');
@@ -42,7 +46,6 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   }
 
-  // Función para manejar clic en botones de flecha hacia abajo
   function handleClickDown(event) {
       event.preventDefault();
       const container = event.target.closest('.user-box');
@@ -75,26 +78,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const rounded = Math.round(value);
 
     if (decimal < 0.25) {
-        return Math.floor(value); // Redondea hacia abajo si la parte decimal es menor que 0.25
+        return Math.floor(value);
     } else if (decimal > 0.75) {
-        return Math.ceil(value); // Redondea hacia arriba si la parte decimal es mayor que 0.75
+        return Math.ceil(value);
     } else if (decimal > 0.25 && decimal < 0.5) {
-        return Math.floor(value) + 0.5; // Redondea al entero más cercano si está entre 0.25 y 0.75
+        return Math.floor(value) + 0.5;
     } else if (decimal < 0.75 && decimal > 0.5) {
-        return Math.ceil(value) - 0.5; // Redondea al entero más cercano si está entre 0.25 y 0.75
+        return Math.ceil(value) - 0.5;
   }
 }
 
-  // Obtener todos los botones de clase 'up' y 'down'
   const upButtons = document.querySelectorAll('.up');
   const downButtons = document.querySelectorAll('.down');
 
-  // Agregar event listener a cada botón de flecha hacia arriba
   upButtons.forEach(function(button) {
       button.addEventListener('click', handleClickUp);
   });
 
-  // Agregar event listener a cada botón de flecha hacia abajo
   downButtons.forEach(function(button) {
       button.addEventListener('click', handleClickDown);
   });
